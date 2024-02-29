17f126cfd96787ffad26c9223afa92acab8a4458
Mert Osanmaz <mosanmaz@danusports.com>
2024-02-29 17:33:01 +0000

    fix:hydration issue

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,10 @@ interface AppState {
   input: string;
   image: string | null;
   sceneData: string | null;
-  excalidrawAPI: any | null; 
+  excalidrawAPI: any | null;
+  isClient:Boolean;
+  Comp: any;
+  excalidrawExportFns: any;
 }
 
 interface Action {
@@ -23,6 +26,9 @@ const initialState: AppState = {
   image: null,
   sceneData: null,
   excalidrawAPI: null,
+  isClient: false,
+  excalidrawExportFns:null,
+  Comp:null
 };
 
 const actionTypes = {
@@ -30,6 +36,9 @@ const actionTypes = {
   SET_IMAGE: 'SET_IMAGE',
   SET_SCENE_DATA: 'SET_SCENE_DATA',
   SET_EXCALIDRAW_API: 'SET_EXCALIDRAW_API',
+  SET_ISCLIENT: 'SET_ISCLIENT',
+  SET_COMP:'SET_COMP',
+  SET_EXCALIDRAWEXPORTFNS:'SET_EXCALIDRAWEXPORTFNS',
 };
 
 fal.config({
@@ -47,6 +56,12 @@ function reducer(state: AppState, action: Action): AppState {
       return { ...state, sceneData: action.payload };
     case actionTypes.SET_EXCALIDRAW_API:
       return { ...state, excalidrawAPI: action.payload };
+    case actionTypes.SET_ISCLIENT:
+      return {...state, isClient: action.payload }
+    case actionTypes.SET_COMP:
+      return {...state, Comp: action.payload }
+    case actionTypes.SET_EXCALIDRAWEXPORTFNS:
+       return {...state, excalidrawExportFns: action.payload }
     default:
       return state;
   }
@@ -54,6 +69,18 @@ function reducer(state: AppState, action: Action): AppState {
 
 export default function Home() {
   const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    import('@excalidraw/excalidraw').then((comp) => dispatch({ type: actionTypes.SET_COMP, payload:comp.Excalidraw }))
+  }, [])
+  useEffect(() => { dispatch({ type: actionTypes.SET_ISCLIENT, payload:true }) }, [])
+  useEffect(() => {
+    import('@excalidraw/excalidraw').then((module) =>
+    dispatch({ type: actionTypes.SET_EXCALIDRAWEXPORTFNS, payload:{
+      exportToBlob: module.exportToBlob,
+      serializeAsJSON: module.serializeAsJSON
+    } 
+  }));
+  }, []);
 
   const { send } = fal.realtime.connect('110602490-sdxl-turbo-realtime', {
     connectionKey: 'realtime-app',
@@ -119,17 +146,19 @@ export default function Home() {
       }
     }
   };
-
+  console.log(state.excalidrawExportFns)
   return (
     <main className="p-12">
       <p className="text-xl mb-2">Fal SDXL Turbo</p>
       <input className='border rounded-lg p-2 w-full mb-2' value={state.input} onChange={handleChange} />
       <div className='flex'>
         <div className="w-[550px] h-[570px]">
-          <Excalidraw
-          excalidrawAPI={(api) => dispatch({ type: actionTypes.SET_EXCALIDRAW_API, payload: api })}
-            onChange={ async(e,appState)=>  handleExcalidrawChange(e,appState)}
-          />
+          {state.isClient && state.excalidrawExportFns && (
+            <state.Comp
+            excalidrawAPI={(api: any) => dispatch({ type: actionTypes.SET_EXCALIDRAW_API, payload: api })}
+            onChange={ async(e: readonly ExcalidrawElement[],appState: any)=>  handleExcalidrawChange(e,appState)}/>
+          )}
+
         </div>
         {state.image && (
           <Image
